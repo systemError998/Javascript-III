2b4cedea536ff480f98c0038581e0cceb7ca2078
systemError998 <annacerasoli998@gmail.com>
2023-12-08 23:53:16 +0100

    f btn , sass

--- 3 - Libreria/script/script.js
+++ 3 - Libreria/script/script.js
@@ -1,34 +1,25 @@
 //Url a cui fare chiamata
 let urlLibreria = 'https://striveschool-api.herokuapp.com/books'
-
 //request , oggetto in grado di fare chiamate 
-let xhr = new XMLHttpRequest()
-
-let arrayLibri = []
-
+let request = new XMLHttpRequest()
 //definisco metodo chiamata , url da chiamare
-xhr.open('GET', urlLibreria)
+request.open('GET', urlLibreria)
 //Eseguo la chiamata
-xhr.send()
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log("completata");
-        let json = JSON.parse(xhr.response)
-        console.log(json);
-        // console.log(json[0].title);
-       /*  json.forEach(libro => {
-            arrayLibri.push(libro)
-        }); */
-        // console.log(arrayLibri);
+request.send()
+request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+        // console.log(request.status);
+        //console.log(this.readyState);
+        console.log("Request completata");
+        let json = JSON.parse(request.response)
+        // console.log(json);
         createCard(json)
-    }
+    } 
 }
-
-// console.log(arrayLibri);
 
 function createCard(libri) {
     let booksSpace = document.querySelector(".row")
-    console.log(booksSpace);
+    // console.log(booksSpace);
     // console.log(libri);
     for(let i=0; i<libri.length; i++) {
         let div = document.createElement("div")
@@ -40,30 +31,49 @@ function createCard(libri) {
                     <div class="card-body">
                         <h5 class="card-title">${libri[i].title}</h5>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">${libri[i].category}</li>
-                        <li class="list-group-item">${libri[i].price}</li>
+                    
+
+                    <ul class="price list-group list-group-flush">
+                        <li class="list-group-item category">${libri[i].category} <span class="float-end"> € ${libri[i].price}</span> </li>
                     </ul>
-                    <div class="card-body">
-                        <a href="#" class="btn btnShop card-link">"Compra Ora"</a>
-                        <a href="#" class="btn btnDelete card-link">"Scarta"</a>
+                    <div class="card-body d-flex justify-content-between">
+                        <a href="#" class="btn btnShop card-link">Compra Ora</a>
+                        <a href="#" class="btn btnDelete card-link"><i class="bi bi-trash-fill"></i></a>
                     </div>
                 </div> `
-                
-                // console.log(btnDelete);
-                let btnDelete = document.querySelectorAll(".btnDelete")
-                console.log(btnDelete);
-                btnDelete[i].addEventListener('click', (b) => {
-                    console.log(b.target);
-                    b.target.parentNode.parentNode.parentNode.classList.add("d-none")
-                    })
+    }   
 
-                let btnShop = document.querySelectorAll(".btnShop")
-                btnShop[i].addEventListener('click', (s) => {
-                    console.log(s.target);
-                    localStorage.setItem("title", s.title)
-                    })
-    }
+
+        let btnDelete = document.querySelectorAll(".btnDelete")
+        // console.log(btnDelete);
+        btnDelete.forEach(btn => {
+            btn.addEventListener('click', (d) => {
+                console.log(d.target);
+                d.target.parentNode.parentNode.parentNode.classList.add("d-none")
+            })
+        });
+
+        //Button COMPRA ORA
+        let btnShop = document.querySelectorAll(".btnShop")
+        //console.log(btnShop);
+        let shopItems = document.querySelector(".modal-body")
+        // console.log(shopItems);
+        // shopItems.innerText = "bella pe te"
+        btnShop.forEach((btn, index) => {
+            btn.addEventListener('click', (s) => {
+                s.preventDefault()
+                sessionStorage.setItem(`title${index}`, libri[index].title)
+                let items = document.createElement("p")
+                items.classList.add("titleBookShop")
+                shopItems.appendChild(items)
+                items.innerHTML = ` ${libri[index].title} 
+                                    <span class="priceBookShop"> €${libri[index].price} </span> ` 
+            // CREARE MESSAGGIO DI AGGIUNTA AL CARRELLO
+                /* let alert = document.createElement("div").classList.add("alert")
+                btn.appendChild("alert") */
+
+            //creare tot carrello
+            })
+        });
+
 }
-
-
